939ab1c356a476693d55d59ea0197607da7abbaf
Parker Rudd <parkerbrudd@gmail.com>
2022-08-10 22:08:33 -0600

    completed get token functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getTokenFromUrl, loginUrl } from "../spotify";
 import '../styles/styles.css';
 
 function Login() {
@@ -6,7 +7,7 @@ function Login() {
     return (
         <div className="login">
             <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="Spotify Logo" />
-            <a>Login with Spotify</a>
+            <a href={loginUrl}>Login with Spotify</a>
         </div>
     )
 }
---
+++ client/src/spotify.js
@@ -0,0 +1,29 @@
+"https://developer.spotify/documentation/web-playback-sdk/quick-start/#"
+
+export const authEndpoint = "https://accounts.spotify.com/authorize"
+
+const redirectUri = "http://localhost:3000/"
+
+const clientId = "024a0534cbc64ad899014f6b4faa4752"
+
+const scopes = [
+    "user-read-currently-playing", 
+    "user-read-recently-played", 
+    "user-read-playback-state", 
+    "user-top-read", 
+    "user-modify-playback-state"
+]
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=') 
+            initial[parts[0]] = decodeURIComponent(parts[1])
+
+            return initial
+        }, {})
+}

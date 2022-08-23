559d3f5181fa9d24c413008eb26363101c40323c
Parker Rudd <parkerbrudd@gmail.com>
2022-08-22 22:00:55 -0600

    Added playlists sidebarOption

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -14,6 +14,14 @@ function Sidebar() {
             <SidebarOption Icon={HomeIcon} title="Home" />
             <SidebarOption Icon={SearchIcon} title="Search" />
             <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+
+            <SidebarOption title="Hip Hop" />
+            <SidebarOption title="Rock" />
+            <SidebarOption title="R&B" />
         </div>
     )
 }

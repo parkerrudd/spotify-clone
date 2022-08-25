068bbfbfb5366fecfa152b1050bd0f9b165da5af
Parker Rudd <parkerbrudd@gmail.com>
2022-08-24 20:47:14 -0600

    styled footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,13 +5,22 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import RepeatIcon from '@mui/icons-material/Repeat';
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+
 
 function Footer() {
 
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+                <img className="footer__albumLogo" src="" alt="Album art" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
 
             <div className="footer__center">
@@ -19,11 +28,22 @@ function Footer() {
                 <SkipPreviousIcon className="footer__icon" />
                 <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
                 <SkipNextIcon className="footer__icon" />
-                <RepeatIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
             </div>
 
             <div className="footer__right">
-                <p>Volume controls</p>
+                <Grid container spacing={2}> 
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+
             </div>
         </div> 
     )
